fe4639e4fd11edf7f6977a7588579677ed8b4d2e
Dan Stoian <dan.stoian@creative-eye.ro>
2018-11-14 12:00:18 +0200

    first commit

---
+++ cookie-policy.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Cookie Policy</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+
+<div id="content">
+
+<h1>Cookie Policy</h1>
+
+<h2>Cookies</h2>
+
+<p>A cookie is a small text file which is automatically saved on your computer when you visit our website and is used to
+    help track activity across a website. [WEBSITE NAME] uses cookies on our website for the following purposes:</p>
+
+<h3>Google Analytics</h3>
+
+<p>We use Google Analytics to help provide anonymous web analytics for our website.</p>
+
+<table border="0">
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Expiration</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>_ga</td>
+        <td>Used to distinguish users</td>
+        <td>2 years</td>
+    </tr>
+    <tr>
+        <td>_gat</td>
+        <td>Used to throttle request rate</td>
+        <td>30 minutes</td>
+    </tr>
+    </tbody>
+</table>
+
+<h3>Website</h3>
+
+<p>We use cookies to help increase the user experience of this website.</p>
+
+<table border="0">
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Expiration</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>seen-cookie-message</td>
+        <td>Used to indicate you have seen the cookie message</td>
+        <td>30 days</td>
+    </tr>
+    </tbody>
+</table>
+
+<h2>Removing cookies</h2>
+<p>If you have any concerns about cookies, or want to remove cookies from your computer, please visit <a href="http://www.allaboutcookies.org/">All About Cookies</a> which explains how to do this.</p>
+
+<h2>Collection of personal information</h2>
+
+<p>If you contact us via a form on our website your personal information will only be used for us to contact you.
+    No personal data you send us will be disclosed to a third party without your permission.</p>
+
+<p>If you wish us to remove any personal details we hold about you, please email us at <a href="mailto:[EMAIL ADDRESS]">[EMAIL ADDRESS]</a></p>
+
+
+</div>
+
+<div id="cookie-message" data-cookie-expiry="60">
+    We use cookies to ensure that we give you the best experience on our website. If you continue to use this site we will assume that you are happy with it.
+    <a href="cookie-policy.php" title="Privacy policy" rel="noindex">Privacy policy</a>
+    <button id="cookie-btn" onclick="closeCookieMessage()">OK</button>
+</div>
+
+<script src="cookie-message.js"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Cookie Message</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+
+<div id="content">
+
+    <h1>Content</h1>
+
+    <p>Dignissimos incididunt tristique, risus platea provident? Eos quibusdam vel euismod, porta cras etiam dolor curabitur dapibus etiam excepturi officia quia. Justo voluptatibus amet morbi bibendum sed consectetur eligendi officiis montes similique curae ratione?</p>
+
+    <p><a href="cookie-policy.php" title="View our Cookie Policy">Find out more about cookies</a></p>
+
+</div>
+
+
+<div id="cookie-message" data-cookie-expiry="60">
+    We use cookies to ensure that we give you the best experience on our website. If you continue to use this site we will assume that you are happy with it.
+    <a href="cookie-policy.php" title="Privacy policy" rel="noindex">Privacy policy</a>
+    <button id="cookie-btn" onclick="closeCookieMessage()">OK</button>
+</div>
+
+<script src="cookie-message.js"></script>
+
+</body>
+</html>
